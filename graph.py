0dd2b62737f5f9e08c715d256ff234c3522dbfa8
Get <villegas.get@gmail.com>
2024-12-19 21:33:54 -0600

    Adding plot

--- graph.py
+++ graph.py
@@ -9,14 +9,12 @@ def plot_initial_data(A, B):
         plt.scatter(b[0], b[1], color='blue')
     
     # Saving the plot
-    plt.savefig("initial_data.png")
-    
-    plt.show()
+    plt.savefig('results/' + "initial_data.png")
     
     return
 
 # Plotting the final decision boundary
-def plot_final_data(A, B, W):
+def plot_final_data(A, B, W, plot_name):
     for a in A:
         plt.scatter(a[0], a[1], color='red')
     for b in B:
@@ -27,8 +25,29 @@ def plot_final_data(A, B, W):
     plt.plot(x, y, color='black')
     
     # Saving the plot
-    plt.savefig("final_data.png")
+    plt.savefig('results/' + plot_name + ".png")
     
-    plt.show()
+    plt.close()
+    
+    return
+
+# Plotting the final decision boundary with new points
+def plot_final_data_with_new_points(A, B, W, new_points, plot_name):
+    for a in A:
+        plt.scatter(a[0], a[1], color='red')
+    for b in B:
+        plt.scatter(b[0], b[1], color='blue')
+    
+    for point in new_points:
+        plt.scatter(point[0], point[1], color='green')
+    
+    x = np.linspace(0, 10, 100)
+    y = (-W[2] - W[0] * x) / W[1]
+    plt.plot(x, y, color='black')
+    
+    # Saving the plot
+    plt.savefig('results/' + plot_name + ".png")
+    
+    plt.close()
     
     return
--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,47 @@
 import numpy as np
 from data import load_data
-from graph import plot_initial_data, plot_final_data
+from graph import plot_initial_data, plot_final_data, plot_final_data_with_new_points
+
+# Compute the perceptron output
+def perceptron_output(x, W):
+    z = np.dot(x, W)
+    return 1 if z > 0 else 0
+
+# Train the perceptron
+def train_perceptron(data, labels, weights, learning_rate):
+    # While everything is not classified correctly
+    epochs = 0
+    while(True):
+        epochs += 1
+        misclassified = False
+        for i in range(len(data)):
+            x = data[i]
+            y = labels[i]
+            y_hat = perceptron_output(x, weights)
+            
+            if y != y_hat:
+                weights += learning_rate * (y - y_hat) * x
+                print(f"----------")
+                print(f"Updating weights: {weights} + {learning_rate * (y - y_hat) * x}")
+                misclassified = True
+                
+        # Print epoch results
+        print("----------")
+        print("Epoch:", epochs)
+        print("Weights:", weights)
+        print("Misclassified:", misclassified)
+        print("Accuracy:", 1 - np.mean(misclassified))
+        print("Learning rate:", learning_rate)
+        print()
+        print("----------")
+
+        # Plot the decision boundary
+        plot_final_data(A, B, weights, "epoch_" + str(epochs))
+        
+        if not misclassified:
+            break
+
+    return weights
 
 # Load data
 A_points, B_points = load_data()
@@ -14,47 +55,35 @@ B = np.array(B_points)
 data = np.concatenate((A, B))
 labels = np.array([1] * len(A) + [0] * len(B))
 
-# Print data and labels
-print("Data:", data)
-print("Labels:", labels)
-
 # Add bias to the data
-data = np.hstack((data, np.ones((data.shape[0], 1))))
-
-# Print data with bias
-print("Data with bias:", data)
+data = np.c_[data, np.ones(data.shape[0])]
 
 # Initialize weights
-W = np.random.rand(3)
-alpha = 0.1 # Learning rate
+weights = np.array([1.0, 1.0, 1.0])
 
-# Activation function
-def activation_function(x):
-    return 1 if x > 0 else 0
+# Plot initial data
+plot_initial_data(A_points, B_points)
+
+# Plot the initial decision boundary	
+plot_final_data(A_points, B_points, weights, "initial_data")
 
 # Train the perceptron
-for epoch in range(100):
-    error_count = 0
-    for j in range(data.shape[0]):
-        z = np.dot(W, data[j])
-        y_hat = activation_function(z)
-        error = labels[j] - y_hat
-
-        # Update weights
-        if error != 0:
-            W += alpha * error * data[j]
-            error_count += 1
-        
-    if error_count == 0:
-        break
-
-# Show the final weights
-print("Final weights:", W)
-
-# Show number of iterations
-print("Number of iterations:", epoch + 1)
+weights = train_perceptron(data, labels, weights, 0.4)
 
 # Plot the final decision boundary
-plot_final_data(A_points, B_points, W)
+plot_final_data(A_points, B_points, weights, "final_data")
 
+# Try to classify a new point
+new_point = np.array([5, 5])
+new_point = np.append(new_point, 1)
+print("New point:", new_point)
+print("Classification:", perceptron_output(new_point, weights))
+plot_final_data_with_new_points(A_points, B_points, weights, [new_point], "final_data_with_new_point1")
+
+
+new_point = np.array([6, 8])
+new_point = np.append(new_point, 1)
+print("New point:", new_point)
+print("Classification:", perceptron_output(new_point, weights))
+plot_final_data_with_new_points(A_points, B_points, weights, [new_point], "final_data_with_new_point2")
 
